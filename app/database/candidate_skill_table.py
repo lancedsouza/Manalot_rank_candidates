21ddd56b8812d99a0a469d07234b191a26f3fc10
Lancelot <lancelot.dsouza@gmail.com>
2026-09-23 10:39:39 +0000

    Changed embedding model

--- app/database/candidate_skill_table.py
+++ app/database/candidate_skill_table.py
@@ -29,7 +29,7 @@ class Candidate_Skill(Base):
     cand_id = Column(Integer, ForeignKey("candidates.id"), nullable=False)
     
     skill = Column(String, nullable=False)
-    skill_embedding = Column(Vector(1024), nullable=False)
+    skill_embedding = Column(Vector(768), nullable=False)
 
     # Use string reference to avoid import loops
     candidate = relationship("Candidate", back_populates="skill_objects")
